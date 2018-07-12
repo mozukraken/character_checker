933969bbbc360bd27950a07b9feeef0c304d4523
mozukraken <mozukraken@gmail.com>
2018-07-13 06:03:29 +0900

    画面遷移を完成させた。

---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  var cards = document.getElementById('cards');
+  var check = document.getElementById('check');
+  var retry = document.getElementById('retry');
+
+  check.addEventListener('click', function() {
+    cards.className = 'move';
+  });
+
+  retry.addEventListener('click', function() {
+    cards.className = '';
+  });
+
+})();
