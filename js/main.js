cd8deaaf16b94bcc4b5b0a6155f6cfd8227e9da3
mozukraken <mozukraken@gmail.com>
2018-07-20 06:34:10 +0900

    診断データの作成をした。

--- js/main.js
+++ js/main.js
@@ -6,6 +6,21 @@
   var retry = document.getElementById('retry');
 
   check.addEventListener('click', function() {
+    var msgs = [
+      '最強の',
+      '最弱の',
+      '最凶の'
+    ];
+    var jobs = [
+      {name: '勇者', img: 'hero.gif'},
+      {name: '魔法使い', img: 'wizard.gif'},
+      {name: '武闘家', img: 'fighter.gif'},
+    ];
+    var types = [
+      {name: 'その炎は全てを焼き尽くす', img: 'bg-fire'},
+      {name: 'その聖水で全てを浄化する', img: 'bg-water'},
+      {name: 'その閃光はこの世の闇を照らす', img: 'bg-light'},
+    ];
     cards.className = 'move';
   });
 
