4daef1ed469727a59409b2b05bd9f6282f93ebac
mozukraken <mozukraken@gmail.com>
2018-07-21 05:47:28 +0900

    診断結果を表示した。

--- js/main.js
+++ js/main.js
@@ -21,6 +21,30 @@
       {name: 'その聖水で全てを浄化する', img: 'bg-water'},
       {name: 'その閃光はこの世の闇を照らす', img: 'bg-light'},
     ];
+
+    var msg;
+    var job;
+    var type;
+
+    var resultImg = document.getElementById('result_img');
+
+    function getRandomElement(array) {
+      return array[Math.floor(Math.random() * array.length)];
+    }
+
+    function setTextContent(id, text) {
+      document.getElementById(id).textContent = text;
+    }
+
+    msg = getRandomElement(msgs);
+    job = getRandomElement(jobs);
+    type = getRandomElement(types);
+
+    setTextContent('result_msg', msg);
+    setTextContent('result_job', job.name);
+    resultImg.children[0].src = 'img/' + job.img;
+    setTextContent('result_type', type.name);
+
     cards.className = 'move';
   });
 
